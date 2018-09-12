7ba92a22be513b873b42d147c22346d6233dd341
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-12 15:43:03 +0200

    Added classes and diagram

---
+++ OOP1/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Person
+    {
+        private string _firstname;
+
+        public string Firstname { get => _firstname; set => _firstname = value; }
+
+        public string Lastname { get; set; }
+
+        public string GetFullName()
+        {
+            return Firstname + " " + Lastname;
+        }
+    }
+}
