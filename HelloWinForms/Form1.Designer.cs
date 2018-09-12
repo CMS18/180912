2b03e08bb46bd86ca964155bb63f0f6351166ce7
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-12 13:09:05 +0200

    Hello Form Demo

--- HelloWinForms/Form1.Designer.cs
+++ HelloWinForms/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddAgeButton = new System.Windows.Forms.Button();
+            this.AvarageAgeLabel = new System.Windows.Forms.Label();
+            this.AgeListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.Location = new System.Drawing.Point(38, 55);
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(434, 31);
+            this.AgeTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ange ålder:";
+            // 
+            // AddAgeButton
+            // 
+            this.AddAgeButton.Location = new System.Drawing.Point(38, 93);
+            this.AddAgeButton.Name = "AddAgeButton";
+            this.AddAgeButton.Size = new System.Drawing.Size(182, 52);
+            this.AddAgeButton.TabIndex = 2;
+            this.AddAgeButton.Text = "Lägg till";
+            this.AddAgeButton.UseVisualStyleBackColor = true;
+            this.AddAgeButton.Click += new System.EventHandler(this.AddAgeButton_Click);
+            // 
+            // AvarageAgeLabel
+            // 
+            this.AvarageAgeLabel.AutoSize = true;
+            this.AvarageAgeLabel.Location = new System.Drawing.Point(43, 173);
+            this.AvarageAgeLabel.Name = "AvarageAgeLabel";
+            this.AvarageAgeLabel.Size = new System.Drawing.Size(173, 25);
+            this.AvarageAgeLabel.TabIndex = 3;
+            this.AvarageAgeLabel.Text = "Medelvärde: ???";
+            // 
+            // AgeListBox
+            // 
+            this.AgeListBox.FormattingEnabled = true;
+            this.AgeListBox.ItemHeight = 25;
+            this.AgeListBox.Location = new System.Drawing.Point(563, 55);
+            this.AgeListBox.Name = "AgeListBox";
+            this.AgeListBox.Size = new System.Drawing.Size(286, 354);
+            this.AgeListBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(903, 450);
+            this.Controls.Add(this.AgeListBox);
+            this.Controls.Add(this.AvarageAgeLabel);
+            this.Controls.Add(this.AddAgeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AgeTextBox);
+            this.Name = "Form1";
+            this.Text = "Hej WinForms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddAgeButton;
+        private System.Windows.Forms.Label AvarageAgeLabel;
+        private System.Windows.Forms.ListBox AgeListBox;
     }
 }
 
--- HelloWinForms/Form1.cs
+++ HelloWinForms/Form1.cs
@@ -12,9 +12,52 @@ namespace HelloWinForms
 {
     public partial class Form1 : Form
     {
+        private static readonly Random randomGenerator = new Random();
+
+        private Button myButton;
+
+        private int sum;
+        private int count;
+
         public Form1()
         {
             InitializeComponent();
+
+            myButton = new Button();
+            myButton.Text = "Klicka på mig!";
+            myButton.Click += MyButton_Click;
+
+            //myButton.Parent = this;
+            //this.Controls.Add(myButton);
+        }
+
+        private void MyButton_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("HEJ! Du klickade på knappen.");
+
+            myButton.Left = randomGenerator.Next(0, this.Width - myButton.Width);
+            myButton.Top = randomGenerator.Next(0, this.Height - myButton.Height);
+
+
+        }
+
+        private void AddAgeButton_Click(object sender, EventArgs e)
+        {
+            int age = int.Parse(AgeTextBox.Text);
+
+            sum = sum + age;
+            count = count + 1;
+
+            double avarage = (double)sum / count;
+
+            AvarageAgeLabel.Text = $"Medelvärde: {avarage:0.00}";
+
+            AgeListBox.Items.Add(age);
+
+            AgeTextBox.Clear();
+            AgeTextBox.Focus();
+
+
         }
     }
 }
